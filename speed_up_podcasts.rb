01a2ad64c6b106dc373f4851626badb32636a5bb
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2008-06-01 16:42:52 +1000

    basically functional units

--- speed_up_podcasts.rb
+++ speed_up_podcasts.rb
@@ -1,70 +1,15 @@
 #!/usr/bin/env ruby
 
 # Increase the tempo of all tracks in the 'podcasts' playlist in iTunes
-# (don't double up - running this multiple times shouldn't hurt)
+# (update state is stored in running this multiple times shouldn't hurt)
 #
 # Dependencies:
 #   (you can get MacPorts from http://www.macports.org/, which'll make the port command work)
 #   sudo port install soundtouch ruby rb-rubygems
 #   sudo gem install activerecord activesupport rb-appscript
-%w[rubygems activerecord activesupport appscript].each {|l| require l }
+%w[rubygems activesupport appscript].each {|l| require l }
 
-WORKING_DIR = '/Users/matt/.podcasts'
-# Database
-DB_FILE = WORKING_DIR + '/db.sqlite3'
-ActiveRecord::Base.establish_connection(:adapter  => 'sqlite3',
-                                        :database => DB_FILE)
-
-# Database migration
-class InitializePodcastTable < ActiveRecord::Migration
-  class << self
-    def up
-      create_table :podcasts do |t|
-        t.string :path, :null => false
-        t.string :state
-      end
-    end
-    def down
-      drop_table :podcasts
-    end
-
-    def current_version
-      begin
-        ActiveRecord::Base.connection.select_value("SELECT version FROM schema_info").to_i
-      rescue
-        0
-      end
-    end
-
-    def migrate_with_schema_changes_and_db(direction = :up)
-      unless File.exist?(WORKING_DIR)
-        FileUtils.mkdir_p(WORKING_DIR)
-      end
-      unless File.exist?(DB_FILE)
-        cmd = "echo '' | sqlite3 '#{DB_FILE}'"
-        puts cmd
-        puts `#{cmd}`
-      end
-      unless current_version == 1
-        migrate_without_schema_changes_and_db(direction)
-        ActiveRecord::Base.connection.update("UPDATE schema_info SET version = 1")
-      end
-    end
-    alias_method_chain :migrate, :schema_changes_and_db
-  end
-end
-InitializePodcastTable.migrate(:up)
-
-# Podcasts and conversion state
-class Podcast < ActiveRecord::Base
-  # lame --decode <mp3> <slow-wav>
-  # soundstretch <slow-wav> <fast-wav> -tempo=+70
-  # rm <slow-wav>
-  # lame -h <fast-wav> <fast-mp3>
-  # rm <fast-wav>
-  # id3cp <mp3> <fast-mp3>
-  # mv <fast-mp3> <mp3>
-  # touch iTunes db for changes
+class Podcast
   class << self
     def iTunes
       @itunes ||= Appscript.app("iTunes")
@@ -80,6 +25,15 @@ class Podcast < ActiveRecord::Base
         yield podcast if podcast.slow?
       end
     end
+    def count
+      all_podcast_refs.size
+    end
+    def all_podcasts
+      all_podcast_refs.collect {|p| new(p) }
+    end
+    def all_slow_podcasts
+      all_podcasts.select {|p| p.slow? }
+    end
 
     protected
 
@@ -88,11 +42,124 @@ class Podcast < ActiveRecord::Base
     end
   end
 
-  def initialize(podcast_ref)
+  # Instance methods
+
+  def initialize(podcast_ref, speedup = 70)
     @podcast_ref = podcast_ref
+    self.speedup = speedup
+  end
+
+  def ref
+    @podcast_ref
+  end
+
+  # Attributes
+  %w[name comment].each do |prop|
+    define_method(prop.to_sym) do
+      ref.send(:"#{prop}").get
+    end
+    define_method(:"#{prop}=") do |value|
+      ref.send(:"#{prop}").set(value)
+    end
+  end
+  def location
+    ref.location
+  end
+  def path
+    location.get.path
+  end
+  def safe_path
+    escape_filename(path)
   end
 
   def slow?
-    @podcast_ref
+    not comment.match(/\{\{\{change_tempo:\+\d+\}\}\}/)
+  end
+
+  attr_accessor :speedup
+  def speedup=(value)
+    raise(ArgumentError, "speedup should be an integer percentage above 0.") unless value.respond_to?(:to_i) && value.to_i >= 1
+    @speedup = value.to_i
+  end
+
+  def change_tempo(cent = self.speedup)
+    begin
+      slow_wav = to_slow_wav
+      fast_wav = soundstretch(slow_wav, cent)
+      cleanup(slow_wav)
+      fast_mp3 = to_mp3(fast_wav)
+      cleanup(fast_wav)
+      copy_tags_to(fast_mp3)
+      overwrite_self_with(fast_mp3)
+      update_comment_with_speedup
+    ensure
+      cleanup(slow_wav, fast_wav, fast_mp3)
+    end
+  end
+
+  protected
+
+  def to_slow_wav
+    # lame --decode <mp3> <slow-wav>
+    filename = tempfile_path("slow.wav")
+    cmd("lame --decode #{safe_path} #{filename}")
+    return filename
+  end
+  def soundstretch(wav, cent)
+    # soundstretch <slow-wav> <fast-wav> -tempo=+70
+    filename = tempfile_path("fast.wav")
+    cmd("soundstretch #{wav} #{filename} -tempo=+#{cent}")
+    return filename
+  end
+  def to_mp3(wav)
+    # lame -h <fast-wav> <fast-mp3>
+    filename = tempfile_path("fast.mp3")
+    cmd("lame -h #{wav} #{filename}")
+    return filename
+  end
+  def copy_tags_to(mp3)
+    # id3cp <mp3> <fast-mp3>
+    cmd("id3cp #{safe_path} #{mp3}")
+    return nil
+  end
+  def overwrite_self_with(mp3)
+    # mv <fast-mp3> <mp3>
+    FileUtils.mv(unescape_filename(mp3), path)
+    return nil
+  end
+  def update_comment_with_speedup
+    # update comment and ensure itunes has noticed new file
+    # touch iTunes db for changes
+    new_comment = self.comment
+    unless slow?
+      raise RuntimeError, "Erm... it looks like we've just double sped up a podcast. Sorry about that."
+    else
+      new_comment << "{{{change_tempo:+#{speedup}}}}"
+    end
+    self.comment = new_comment
+    return new_comment
+  end
+  def cleanup(*files)
+    # rm <file>
+    files.each do |file|
+      FileUtils.rm(file) if File.exist?(file)
+      FileUtils.rm(unescape_filename(file)) if File.exist?(unescape_filename(file))
+    end
+  end
+
+  private
+
+  def tempfile_path(ext)
+    escape_filename(Tempfile.new("#{object_id}_#{ext}").path)
+  end
+  def escape_filename(filename)
+    filename.gsub(/([-+ ])/,'\\\\\1')
+  end
+  def unescape_filename(filename)
+    filename.gsub(/\\/,'')
+  end
+  def cmd(command_string)
+    puts command_string
+    puts `#{command_string}`
   end
 end

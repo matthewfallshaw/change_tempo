cb9bd5c7982fffdb3fc13ff7fbdb94cb65990bbe
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2009-04-17 16:48:52 +1000

    add README

--- change_tempo.rb
+++ change_tempo.rb
@@ -1,21 +1,5 @@
 #!/usr/bin/env ruby
 
-# Increase the tempo of all tracks in the 'Podcasts' playlist in iTunes
-# (update state is stored in the mp3 'comments' tag, and already altered tracks
-# will not be re-fiddled, so running this multiple times shouldn't hurt)
-#
-# === Dependencies:
-#
-# * sudo port install soundtouch id3lib ruby rb-rubygems
-# ** (you can get MacPorts from http://www.macports.org/, which'll make the port command work)
-# * sudo gem install activesupport rb-appscript
-#
-# === Install:
-# 
-# Put this somewhere sensible (like ~/bin/change_tempo) and run it regularly, by, say:
-#   crontab -e
-#   15 3 * * * ~/bin/change_tempo --speedup 20 --playlist new-podcasts > ~/log/change_tempo.log
-
 %w[rubygems activesupport appscript tempfile].each {|l| require l }
 
 class Podcast

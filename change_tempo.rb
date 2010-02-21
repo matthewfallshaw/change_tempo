769e40a86754a129bfc10cf4e0f88daa48194a55
Matthew Fallshaw <matthew.fallshaw@gmail.com>
2010-02-22 08:46:05 +1100

    update: active_support preferred in newer versions

--- change_tempo.rb
+++ change_tempo.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-%w[rubygems activesupport appscript tempfile].each {|l| require l }
+%w[rubygems active_support appscript tempfile].each {|l| require l }
 file = __FILE__
 file = File.expand_path(File.readlink(file), File.dirname(file)) while File.symlink?(file)
 Dir[File.join(File.dirname(file), "lib", "*")].each { |l| require l }
